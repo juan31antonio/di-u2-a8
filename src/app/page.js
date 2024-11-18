26b8ef07946d583230ff1df7181b56ed8d4b3cd7
juan31antonio <juanantoniocorder32@gmail.com>
2024-11-18 10:13:37 +0100

    finalizado

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,7 @@ export default function TrafficLight() {
 
   function handleClick() {
     setWalk(!walk);
+    alert(walk ? 'El siguiente estado es parar' : 'El siguiente estado es caminar')
   }
 
   return (
